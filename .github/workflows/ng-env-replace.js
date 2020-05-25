fe3f4901b5b84c92d7c6365cfe5040516484ddec
Francesco Sciuti <fsciuti@gmail.com>
2020-05-25 17:57:54 +0200

    Update ng-env-replace.js

--- .github/workflows/ng-env-replace.js
+++ .github/workflows/ng-env-replace.js
@@ -5,7 +5,7 @@ const file = fs.readFileSync(targetPath ,'utf8');
 
 console.log(file);
 
-const newFile = file.replace('REDIS_SERVER_API_WS', '${process.env.REDIS_SERVER_API_WS}');
+const newFile = file.replace('REDIS_SERVER_API_WS', process.env.REDIS_SERVER_API_WS);
 
 console.log(newFile);
 
